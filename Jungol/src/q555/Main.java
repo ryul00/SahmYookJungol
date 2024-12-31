e545d40dd5869ecbf2e32359be3c6cae1da9c891
유광렬 <129136966+ryeol00@users.noreply.github.com>
2024-12-31 11:20:12 +0900

    bf,bw추가 q555

--- Jungol/src/q555/Main.java
+++ Jungol/src/q555/Main.java
@@ -1,10 +1,12 @@
 package q555;
 
 import java.util.Scanner;
+import java.io.*;
 
 public class Main {
 
-	public static void main(String[] args) {
+	// Scanner 사용 
+	public static void main(String[] args)throws IOException {
 	Scanner sc = new Scanner(System.in);
 	String[] ar = new String[10]; // 배열 생성, [요소개수=배열크기]
 	for(int i = 0; i < ar.length; i++ ) {
@@ -12,6 +14,16 @@ public class Main {
 		System.out.print(ar[i]);
 	}
 	sc.close();
+	
+	// BufferedReader / BufferedWriter 사용
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    String input[] = br.readLine().split(" ");
+    for(int i=0;i < input.length;i++){
+        bw.write(String.valueOf(input[i]));
+    }
+    bw.flush();
+	bw.close();
 			
 	}
 
