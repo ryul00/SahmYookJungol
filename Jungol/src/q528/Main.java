dee4ad54f5bd264640554b605d10469cfd60e9f7
유광렬 <129136966+ryeol00@users.noreply.github.com>
2024-12-31 09:10:05 +0900

    q536풀이

--- Jungol/src/q528/Main.java
+++ Jungol/src/q528/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in); // scanner 선언 시 끝에 close()메서드로 종료 필요
+		Scanner sc = new Scanner(System.in); 
 	    int n = sc.nextInt();   
 	    sc.close();
 	    
@@ -15,5 +15,7 @@ public class Main {
 	    }
 	 
 	}
+	
+	
 
 }
---
+++ Jungol/src/q536/Main.java
@@ -0,0 +1,14 @@
+package q536;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int n = 1;
+		while (n <= 15) {
+			System.out.println(n);
+			n++;
+		}
+
+	}
+
+}
