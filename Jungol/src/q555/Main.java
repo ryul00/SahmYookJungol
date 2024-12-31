243255a56f9b2d5b288ab32dbf89aa019d2fd6b4
유광렬 <129136966+ryeol00@users.noreply.github.com>
2024-12-31 11:01:11 +0900

    q555 완료

--- Jungol/src/q555/Main.java
+++ Jungol/src/q555/Main.java
@@ -6,9 +6,9 @@ public class Main {
 
 	public static void main(String[] args) {
 	Scanner sc = new Scanner(System.in);
-	String[] s = new String[10];
+	String[] s = new String[10]; // 배열 생성, [요소개수=배열크기]
 	for(int i = 0; i < 10; i++ ) {
-		s[i] = sc.next();
+		s[i] = sc.next(); // 입력받은 문자열을 배열에 저장한다.
 		System.out.print(s[i]);
 	}
 	sc.close();
