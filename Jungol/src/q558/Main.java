6477e610b4a1b52582e4c06b253f6740859db9b4
유광렬 <129136966+ryeol00@users.noreply.github.com>
2024-12-31 14:25:08 +0900

    558완료

--- Jungol/src/q558/Main.java
+++ Jungol/src/q558/Main.java
@@ -3,23 +3,20 @@ package q558;
 import java.util.Scanner;
 
 public class Main {
-
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
 		int[] inp = new int[100];
+		Scanner sc = new Scanner(System.in);
 		int idx = 0;
-		for(;;) {
+		for (;;) {
 			inp[idx] = sc.nextInt();
 			if (inp[idx++] == 0) {
 				sc.close();
 				break;
 			}
 		}
-		for(int i = idx - 2; i >= 0; i--) {
+
+		for (int i = idx - 2; i >= 0; i--) {
 			System.out.print(inp[i] + " ");
 		}
-
 	}
-
 }
