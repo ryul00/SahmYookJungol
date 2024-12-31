5e752cc21c08e0efd5f1ca52e03d7e6802ebd529
유광렬 <129136966+ryeol00@users.noreply.github.com>
2024-12-31 11:11:43 +0900

    q555 풀이 완료

--- Jungol/src/q555/Main.java
+++ Jungol/src/q555/Main.java
@@ -6,10 +6,10 @@ public class Main {
 
 	public static void main(String[] args) {
 	Scanner sc = new Scanner(System.in);
-	String[] s = new String[10]; // 배열 생성, [요소개수=배열크기]
-	for(int i = 0; i < 10; i++ ) {
-		s[i] = sc.next(); // 입력받은 문자열을 배열에 저장한다.
-		System.out.print(s[i]);
+	String[] ar = new String[10]; // 배열 생성, [요소개수=배열크기]
+	for(int i = 0; i < ar.length; i++ ) {
+		ar[i] = sc.next(); // 입력받은 문자열을 배열에 저장한다.
+		System.out.print(ar[i]);
 	}
 	sc.close();
 			
