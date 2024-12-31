d394070f035c52ad1d7f1220d6664b764051f4ff
유광렬 <129136966+ryeol00@users.noreply.github.com>
2024-12-31 09:19:10 +0900

    q536 완료

--- Jungol/src/q536/Main.java
+++ Jungol/src/q536/Main.java
@@ -3,12 +3,16 @@ package q536;
 public class Main {
 
 	public static void main(String[] args) {
-		int n = 1;
-		while (n <= 15) {
-			System.out.println(n);
-			n++;
+		for (int i = 1; i <= 15; i++) {
+			System.out.print(i + " ");
 		}
-
+		System.out.println();
+		int n = 1;
+		while(n <= 15) {
+			System.out.print(n++ + " ");
+		}
 	}
+	
+		
 
 }
