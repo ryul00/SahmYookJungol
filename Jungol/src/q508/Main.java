290f6acf756175b1b4c2838cde679b4c2ffe318e
유광렬 <129136966+ryeol00@users.noreply.github.com>
2025-01-03 17:36:13 +0900

    q508 완료

---
+++ Jungol/src/q508/Main.java
@@ -0,0 +1,26 @@
+package q508;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.printf("%10s", "item");
+		System.out.printf("%10s", "count");
+		System.out.printf("%10s", "price");
+		System.out.println();
+		System.out.printf("%10s", "pen");
+		System.out.printf("%10d", 20);
+		System.out.printf("%10d", 100);
+		System.out.println();
+		System.out.printf("%10s", "note");
+		System.out.printf("%10d", 5);
+		System.out.printf("%10d", 95);
+		System.out.println();
+		System.out.printf("%10s", "eraser");
+		System.out.printf("%10d", 110);
+		System.out.printf("%10d", 97);
+		
+	}
+		
+
+}
