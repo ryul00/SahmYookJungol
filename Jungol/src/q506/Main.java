238ff044747a332a770e733b6c3013258f616e80
유광렬 <129136966+ryeol00@users.noreply.github.com>
2025-01-03 16:06:26 +0900

    q506 완성

--- Jungol/src/q506/Main.java
+++ Jungol/src/q506/Main.java
@@ -3,13 +3,14 @@ package q506;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("My height");
-		int h = 170;
-		System.out.println(h);
 		
-		System.out.println("My weight");
+		int h = 170;		
 		double w = 68.600000;
-		System.out.printf("%.6f\n", w); // 실수 자릿수 출력 
+		
+		System.out.println("My height");
+		System.out.println(h);
+		System.out.println("My weight");
+		System.out.printf("%.6f\n", w); // 실수 특정 자릿수 출력 
 		
 
 	}
