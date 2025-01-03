fb2b93a7f7d3482c15f44157db846a1544178875
유광렬 <129136966+ryeol00@users.noreply.github.com>
2025-01-03 15:44:35 +0900

    q506완료

---
+++ Jungol/src/q506/Main.java
@@ -0,0 +1,17 @@
+package q506;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("My height");
+		int h = 170;
+		System.out.println(h);
+		
+		System.out.println("My weight");
+		double w = 68.600000;
+		System.out.printf("%.6f\n", w); // 실수 자릿수 출력 
+		
+
+	}
+
+}
