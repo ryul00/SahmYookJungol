c3b8e3998bbeb71e96d6a2dc138f9fb2e594fec7
유광렬 <129136966+ryeol00@users.noreply.github.com>
2025-01-05 17:40:50 +0900

    q613

--- Jungol/src/q613/Main.java
+++ Jungol/src/q613/Main.java
@@ -2,22 +2,29 @@ package q613;
 
 import java.util.Scanner;
 
-class School {
-	 String name;
-	 String schoolName;
-	 int grade;
+/*"이름", "학교명", "학년"을 입력받아 아래와 같이 출력하는 프로그램을 작성하시오.
+(이름과 학교명은 각각 20자 이하이다.)*/
+
+class Student{
+	String name;
+	String schoolName;
+	int grade;
 }
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        School s = new School();
-        s.name = sc.next();
-        s.schoolName = sc.next();
-        s.grade = sc.nextInt();
 
-        System.out.println("Name : " + s.name);
-        System.out.println("School : " + s.schoolName);
-        System.out.println("Grade : " + s.grade);
-    }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Student s = new Student();
+		
+		s.name = sc.next();
+		s.schoolName = sc.next();
+		s.grade = sc.nextInt();
+		
+		System.out.println("Name : " + s.name);
+		System.out.println("School : " + s.schoolName);
+		System.out.println("Grade : " + s.grade);
+
+	}
+
 }
--- Jungol/src/q613/Student.java
+++
@@ -1,8 +0,0 @@
-package q613;
-
-public class Student {
-	private String name;
-	private int grade;
-	private String school;
-
-}
