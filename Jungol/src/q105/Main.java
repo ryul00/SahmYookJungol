8824e6b816a4a2ef57774c4b54fce4b470fd571d
유광렬 <129136966+ryeol00@users.noreply.github.com>
2025-01-08 12:07:37 +0900

    q105

---
+++ Jungol/src/q105/Main.java
@@ -0,0 +1,33 @@
+package q105;
+//          Seoul     10,312,545        +91,375
+//Pusan      3,567,910         +5,868
+//Incheon      2,758,296        +64,888
+//Daegu      2,511,676        +17,230
+//Gwangju      1,454,636        +29,774
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.printf("%15s", "Seoul");
+		System.out.printf("%15s", "10,312,545");
+		System.out.printf("%15s\n", "+91,375");
+		
+		System.out.printf("%15s", "Pusan");
+		System.out.printf("%15s", "3,567,910");
+		System.out.printf("%15s\n", "+5,868");
+		
+		System.out.printf("%15s", "Incheon");
+		System.out.printf("%15s", "2,758,296");
+		System.out.printf("%15s\n", "+64,888");
+		
+		System.out.printf("%15s", "Daegu");
+		System.out.printf("%15s", "2,511,676");
+		System.out.printf("%15s\n", "+17,230");
+		
+		System.out.printf("%15s", "Gwangju");
+		System.out.printf("%15s", "1,454,636");
+		System.out.printf("%15s\n", "+29,774");
+
+	}
+
+}
