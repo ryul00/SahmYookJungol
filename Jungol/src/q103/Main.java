4728f13d62d513ffcd71fb54c5f91790769d876c
유광렬 <129136966+ryeol00@users.noreply.github.com>
2025-01-09 09:02:06 +0900

    q103

---
+++ Jungol/src/q103/Main.java
@@ -0,0 +1,18 @@
+package q103;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String T1 = "TTTTTTTTTT";
+		String T2 = "TT";
+		for(int i=0; i<2; i++) {
+				System.out.println(T1);
+		}
+		for(int i=0; i<3; i++) {
+			System.out.printf("%6s", T2);
+			System.out.println();
+		}
+
+	}
+
+}
