3cc165a99b47b7adea1941f07faa8d36d33aaddb
유광렬 <129136966+ryeol00@users.noreply.github.com>
2025-01-14 12:24:35 +0900

    Main : 클래스 Main2 : 노가다

--- Jungol/src/q616/Main.java
+++ Jungol/src/q616/Main.java
@@ -2,40 +2,55 @@ package q616;
 
 import java.util.Scanner;
 
-// 세 꼭지점이 (x1 y1), (x2 y2), (x3 y3)인 삼각형의 무게중심 = ((x1+x2+x3)/3 (y1+y2+y3)/3)
+class Triangle {
+    private int[] point1, point2, point3;
 
-class Triangle{
-	double x1,x2,x3,y1,y2,y3;
-	
-	public Triangle(double x1,double x2, double x3, double y1, double y2, double y3) {
-		this.x1 = x1;
-		this.x2 = x2;
-		this.x3 = x3;
-		this.y1 = y1;
-		this.y2 = y2;
-		this.y3 = y3;
-	}
+    public Triangle() {
+        point1 = new int[2];
+        point2 = new int[2];
+        point3 = new int[2];
+    }
+    
+    public void PointInfo() {
+    	Scanner sc = new Scanner(System.in);
+    	for(int i=0; i<2;i++) {
+    		point1[i] = sc.nextInt();
+    	}
+    	for(int i=0; i<2;i++) {
+    		point2[i] = sc.nextInt();
+    	}
+    	for(int i=0; i<2;i++) {
+    		point3[i] = sc.nextInt();
+    	}
+    	sc.close();
+    }
+    
+ 
+    
+    public double getCenterX() {
+    	double centerX = (point1[0] + point2[0] + point3[0]) / 3.0;
+    	return centerX;
+    	
+    }
+    
+    public double getCenterY() {
+    	double centerY = (point1[1] + point2[1] + point3[1]) / 3.0;
+		return centerY;
+    }
+	    
+    
+    
 }
 
 public class Main {
-
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		double x1 = sc.nextDouble();
-		double y1 = sc.nextDouble();
-		double x2 = sc.nextDouble();
-		double y2 = sc.nextDouble();
-		double x3 = sc.nextDouble();
-		double y3 = sc.nextDouble();
-		sc.close();
+	public static void main(String[] args){
 		
-		double center_x = (x1 + x2 + x3) / 3;
-		double center_y = (y1 + y2 + y3) / 3;
-		
+		Triangle tr = new Triangle();
+		tr.PointInfo();
 		System.out.print("(");
-		System.out.print(String.format("%.1f", center_x ));
-		System.out.print("," + " ");
-		System.out.print(String.format("%.1f", center_y));
+		System.out.print(String.format("%.1f", tr.getCenterX() ));
+		System.out.print(", ");
+		System.out.print(String.format("%.1f", tr.getCenterY()));
 		System.out.print(")");
 
 	}
