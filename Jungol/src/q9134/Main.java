d91ac4f4dface2d2aaaf597ab11ef381a8090198
유광렬 <129136966+ryeol00@users.noreply.github.com>
2025-01-18 19:11:03 +0900

    q9134

--- Jungol/src/q9134/Main.java
+++ Jungol/src/q9134/Main.java
@@ -45,5 +45,64 @@ class Point{
 	
 	
 }
+// 이상적인 코드 
+// package q9134;
+//
+// import java.util.Scanner;
+//
+// public class Main {
+//
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        
+//        // 첫 번째 점 입력
+//        System.out.print("첫 번째 점의 좌표는? (x, y) ");
+//        Point p1 = new Point(sc.nextDouble(), sc.nextDouble());
+//
+//        // 두 번째 점 입력
+//        System.out.print("두 번째 점의 좌표는? (x, y) ");
+//        Point p2 = new Point(sc.nextDouble(), sc.nextDouble());
+//        
+//        sc.close();
+//
+//        // 중심점 계산 및 출력
+//        Point center = CenterCalculator.calculateCenter(p1, p2);
+//        System.out.printf("중심점의 위치 = (%.1f, %.1f)%n", center.getX(), center.getY());
+//    }
+//}
+//
+//  하나의 점을 표현하는 클래스
+// class Point {
+//    private double x;
+//    private double y;
+//
+//    // 생성자: 좌표를 입력받아 초기화
+//    public Point(double x, double y) {
+//        this.x = x;
+//        this.y = y;
+//    }
+//
+//    // x 좌표 반환
+//    public double getX() {
+//        return x;
+//    }
+//
+//    // y 좌표 반환
+//    public double getY() {
+//        return y;
+//    }
+//}
+//
+// 중심점 계산하는 클래스
+// class CenterCalculator {
+//
+//    // 두 점의 중심 좌표를 계산하여 Point 객체로 반환
+//    public static Point calculateCenter(Point p1, Point p2) {
+//        double centerX = (p1.getX() + p2.getX()) / 2;
+//        double centerY = (p1.getY() + p2.getY()) / 2;
+//        return new Point(centerX, centerY); // 중심 좌표를 가진 새로운 Point 객체 반환
+//    }
+// }
+
 
 
