9119045c8bca37e06eb65e361e29268e7bf4ba31
유광렬 <129136966+ryeol00@users.noreply.github.com>
2025-01-18 16:19:15 +0900

    q5933

---
+++ Jungol/Beginner/q5933/Main.java
@@ -0,0 +1,28 @@
+package q5933;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int arr[] = new int[n]; 
+		
+		
+		for(int i=0; i<n; i++) {
+			int num = 1;
+			for(int j=0; j<n; j++) {
+				num = num + i;
+				arr[i] = num;
+				System.out.print(num + " ");
+				num++;
+				
+			}
+			
+			System.out.println();
+		}
+
+	}
+
+}
