62a77d40e9b9c5a6fb30fe7971086dcdb7c10b45
유광렬 <129136966+ryeol00@users.noreply.github.com>
2025-01-18 19:09:42 +0900

    q1314

---
+++ Jungol/Beginner/Q1314/Main.java
@@ -0,0 +1,42 @@
+package Q1314;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		char arr[][] = new char[n][n];
+		sc.close();
+		
+		char ch = 'A';
+		for(int i=0; i<n; i++) {
+			if(i % 2 == 0) {
+				for(int j=0; j<n; j++) {
+					arr[j][i] = ch;
+					ch += 1;
+					if(ch > 'Z') {
+						ch = 'A';
+					}
+				}
+			}else {
+				for(int j=n-1; j>=0; j--) {
+					arr[j][i] = ch;
+					ch += 1;
+					if(ch > 'Z') {
+						ch = 'A';
+					}
+				}
+			}
+		}
+
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<n; j++) {
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+
+}
