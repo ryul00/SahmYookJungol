057d98b4169601486cf20fb7d0b244cde346b918
유광렬 <129136966+ryeol00@users.noreply.github.com>
2025-01-21 09:28:54 +0900

    q570

---
+++ Jungol/src/q572/Main.java
@@ -0,0 +1,21 @@
+package q572;
+
+import java.util.Scanner;
+
+public class Main {
+	static double r;
+	
+	public static void Circle(double r) {
+		double result = r * r * 3.14;
+		System.out.printf("%.2f" ,result);
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		r = sc.nextInt();
+		sc.close();
+		Circle(r);
+
+	}
+
+}
