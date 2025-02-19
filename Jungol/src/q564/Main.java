d5d74e5eb5e3ac7ae0a128de437c75c137000ef6
ryul00 <gju07059@gmail.com>
2025-02-19 14:31:25 +0900

    q564

---
+++ Jungol/src/q564/Main.java
@@ -0,0 +1,33 @@
+package q564;
+
+import java.util.Scanner;
+
+public class Main {
+	static char inp;
+	
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int arr[] = new int[26];
+		
+		while(true) {
+			inp = sc.next().charAt(0);; 
+			if (inp < 'A' || inp > 'Z') {
+			    break;  
+			}
+			
+			arr[inp-'A']++;
+			
+		}
+		
+		for(int i=0; i<arr.length; i++) {
+			
+			if(arr[i] > 0) {
+				System.out.println((char)(i+'A') + " : " + arr[i]);
+			}
+		}
+		
+
+	}
+
+}
