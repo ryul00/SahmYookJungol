84efb0f4938752d6384a3d96646ef479c6dbd832
ryul00 <gju07059@gmail.com>
2025-04-10 09:47:36 +0900

    q1338

---
+++ Jungol/Beginner/q1338/Main.java
@@ -0,0 +1,40 @@
+package q1338;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		char arr[][] = new char[N][N];
+		char ch = 'A';
+		for(int i=0; i<N; i++) {
+			for(int j=i,k=N-1; j<N; j++, k--) {
+				arr[j][k] = ch++;
+				if(ch > 'Z') {
+					ch ='A';
+				}
+			}
+		}
+		// 04 13 22 31 40
+		// 14 23 32 41
+		// 24 33 42
+		// 34 43
+		// 44
+		
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<N; j++) {
+				if(arr[i][j] == 0) {
+					System.out.print("  "); // arr[i][j] + " " => A+" " 이므로 칸 당 공백 2개로 설정
+				}else {
+					System.out.print(arr[i][j] + " ");
+				}
+			}
+			System.out.println();
+		}
+		
+
+	}
+
+}
